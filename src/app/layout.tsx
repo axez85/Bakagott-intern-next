54a4c26ea0c7c0fc3c91396a9c0053462a2fbce1
David Aslan <dev@davidaslan.se>
2025-07-29 22:14:36 +0200

    Adding navigation and a page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Topbar from "@/components/topbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header />
+        <Topbar />
         {children}
       </body>
     </html>
