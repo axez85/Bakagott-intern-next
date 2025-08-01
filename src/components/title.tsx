da49f886a8fa01864240a116425a89f44e5bab68
David Aslan <dev@davidaslan.se>
2025-08-01 19:13:26 +0200

    Styling title and navigation with company color.

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,9 +1,9 @@
  
 export default function Title() {
   return (
-    <div className="text-center pt-3 m-16">
-        <div className="max-w-3xl mx-auto bg-blue-200">
-            <h1 className=" text-3xl sm:text-4xl md:text-7xl lg:text-32xl underline italic">
+    <div className="text-center pt-3 m-16 text-white">
+        <div className="max-w-3xl mx-auto bg-[#134978]">
+            <h1 className=" text-3xl sm:text-4xl md:text-7xl lg:text-32xl underline decoration-3 italic">
                 Goddag Bakagott!
             </h1>
             <p className="max-w-3/4 mx-auto leading-10">
