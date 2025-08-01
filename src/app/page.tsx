d70d39affbeb3523af8edea02c090ddbc4d0ef0a
David Aslan <dev@davidaslan.se>
2025-08-01 15:07:51 +0200

    Sperating title to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 export default function Home() {
   return (
-    <div className="text-center pt-37 px-5">
-      <h1 className="md:text-32xl sm:text-8xl text-3xl">
-        Goddag Bakagott!
-      </h1>
-      <p className="max-w-[750px] mx-auto leading-10">
-        Verktyg för att skapa och hantera Etiketter & Följesedlar m.m.  
-      </p>
-
-    </div>
+    <div></div>
   );
 }
---
+++ src/components/title.tsx
@@ -0,0 +1,13 @@
+ 
+export default function Title() {
+  return (
+    <div className="text-center bg-blue-200 pt-3 m-16 max-w-3/4">
+      <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-32xl underline italic">
+        Goddag Bakagott!
+      </h1>
+      <p className="max-w-3/4 mx-auto leading-10">
+        Verktyg för att skapa och hantera Etiketter & Följesedlar m.m.  
+      </p>
+    </div>
+  )
+}
