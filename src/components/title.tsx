7f85b4b8bbfb9626e83e4fbdaf835b55255a0ea4
David Aslan <dev@davidaslan.se>
2025-08-01 16:15:15 +0200

    Styleing the title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,13 +1,15 @@
  
 export default function Title() {
   return (
-    <div className="text-center bg-blue-200 pt-3 m-16 max-w-3/4">
-      <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-32xl underline italic">
-        Goddag Bakagott!
-      </h1>
-      <p className="max-w-3/4 mx-auto leading-10">
-        Verktyg för att skapa och hantera Etiketter & Följesedlar m.m.  
-      </p>
+    <div className="text-center pt-3 m-16">
+        <div className="max-w-3xl mx-auto bg-blue-200">
+            <h1 className=" text-3xl sm:text-4xl md:text-7xl lg:text-32xl underline italic">
+                Goddag Bakagott!
+            </h1>
+            <p className="max-w-3/4 mx-auto leading-10">
+                Verktyg för att skapa och hantera Etiketter & Följesedlar m.m.  
+            </p>
+        </div>
     </div>
   )
 }
