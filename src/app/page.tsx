9d1b364ed146c205c853547194a4dd4e640b9c61
David Aslan <dev@davidaslan.se>
2025-08-11 13:21:28 +0200

    Add a new print page and switch to the optimized way of using forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Form from 'next/form'
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white flex flex-col items-center px-4 py-10">
-      <form className="w-full max-w-3/4 space-y-6">
+      <Form action="/print-label"
+      className="w-full max-w-3/4 space-y-6"> 
         <div>
           <label htmlFor="saved-labels" className="block text-lg font-semibold mb-2">
             Sparade etiketter
@@ -36,7 +39,54 @@ export default function Home() {
             placeholder="Ange underrubrik..."
           />
         </div>
-      </form>
+
+        <div>
+          <label htmlFor="ammount" className="block text-lg font-semibold mb-2">
+            Antal (st)
+          </label>
+          <input
+            type="number"
+            id="ammount"
+            name="ammount"
+            className="w-full border border-gray-300 rounded-lg p-2"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="weight" className="block text-lg font-semibold mb-2">
+            Vikt (gram)
+          </label>
+          <input
+            type="number"
+            id="weight"
+            name="weight"
+            className="w-full border border-gray-300 rounded-lg p-2"
+          />
+        </div>
+        
+
+        <div>
+          <input
+            type="checkbox"
+            id="Checkweight1"
+            name="Checkweight"
+
+            className="w-full border border-gray-300 rounded-lg p-2"
+            checked
+          />
+          <label htmlFor="Checkweight" className="block text-lg  mb-2">
+            Visa Vikt på etiketten
+          </label>
+        </div>
+
+
+              <button 
+                className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full"
+                type="submit">
+                  Visa Etikett
+              </button>
+
+      </Form>
     </div>
   );
 }
---
+++ src/app/print-label/page.tsx
@@ -0,0 +1,13 @@
+
+export default function PrintLabel() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center px-4 py-10">
+      <form className="w-full max-w-3/4 space-y-6">
+          <h1 className="block text-lg font-semibold mb-2">
+            Skrivter ut ettikett
+          </h1>
+      </form>
+    </div>
+  
+  )
+}
